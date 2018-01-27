3ac8f8a64548370d1c5efaae54f778afc0f58998
Paulien-L <34313853+Paulien-L@users.noreply.github.com>
2018-01-27 15:51:47 +0100

    Update Codondictionary.py

--- Codondictionary.py
+++ Codondictionary.py
@@ -1,3 +1,4 @@
+#File used as template (see repository)
 codontable = 'codonCodingTable.txt'
 
 def readCodonTable(file):
@@ -5,9 +6,12 @@ def readCodonTable(file):
         lines = f.read()
         lines = lines.split()
         
+        #List of codons on odd lines
         codons = lines[::2]
+        #list of residues on even lines
         residues = lines[1::2]
         
+        #zip into dictionary with codons as key and residues as value
         trans_dict = dict(zip(codons, residues))
         
         return trans_dict
